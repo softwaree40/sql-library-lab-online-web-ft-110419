0446f777380ac3d64a01b1e9642acdf2491fa25d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-19 06:19:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,6 +31,13 @@ end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
+  "SELECT series.title
+  FROM characters
+  JOIN authors ON characters.author_id = authors.id
+  JOIN series ON series.author_id = authors.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
